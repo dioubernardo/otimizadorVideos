d1c28794fb13d9cec511c0b51dcf851d8087511e
Bernardo Silva <contato@bernardosilva.com.br>
2021-02-08 09:37:07 -0300

    versao beta

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Otimizador_de_vídeos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecionar vídeo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 62);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(665, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(746, 169);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(683, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 272);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Otimizador de vídeos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otimizador_de_vídeos
+{
+    public partial class Form1 : Form
+    {
+
+        Process process;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {
+
+            textBox1.Clear();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+
+                StatusTela(false);
+                textBox1.AppendText("Iniciando processo de otimização\r\n");
+
+                Task.Run(() => {
+
+                    string inputFile = openFileDialog1.FileName;
+                    string extension = Path.GetExtension(inputFile);
+                    string tempFile = Path.GetTempFileName() + extension;
+                    string outputFile = inputFile.Substring(0, inputFile.Length - extension.Length);
+                    outputFile += "-otimizado-em-" + DateTime.Now.ToString("yyyyMMddHHmmss") + extension;
+
+                    ProcessStartInfo statInfo = new ProcessStartInfo
+                    {
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        FileName = Directory.GetCurrentDirectory() + @"\ffmpeg.exe",
+                        Arguments = "-hide_banner -i " + EscapeFile(inputFile) + " " + EscapeFile(tempFile)
+                    };
+
+                    process = new Process
+                    {
+                        StartInfo = statInfo
+                    };
+
+                    process.ErrorDataReceived += process_OutputDataReceived;
+                    process.OutputDataReceived += process_OutputDataReceived;
+                    process.Start();
+
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+
+                    process.WaitForExit();
+
+                    process.CancelOutputRead();
+                    process.CancelErrorRead();
+                    int exitcode = process.ExitCode;
+                    process = null;
+
+                    StatusTela(true);
+
+                    if (exitcode == 0) {
+                        FileInfo infoInput = new FileInfo(inputFile);
+                        FileInfo infoTemp = new FileInfo(tempFile);
+
+                        if (infoInput.Length > infoTemp.Length) {
+                            File.Move(tempFile, outputFile);
+                            MessageBox.Show("Seu vídeo foi salvo na mesma pasta do vídeo original", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        } else {
+                            MessageBox.Show("Desculpe, não conseguimos reduzir o tamanho do seu vídeo", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    } else {
+                        MessageBox.Show("Não foi possível converter seu vídeo", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    if (File.Exists(tempFile))
+                        File.Delete(tempFile);
+                });
+
+            }
+        }
+
+        private string EscapeFile(string file) {
+            return "\"" + ((string)file).Replace(@"\", @"\\").Replace("\"", @"\" + "\"") + "\"";
+        }
+
+        void StatusTela(bool ativo) {
+            this.Invoke((MethodInvoker)delegate {
+                button1.Enabled = ativo;
+                button2.Enabled = !ativo;
+                progressBar1.Style = ativo ? ProgressBarStyle.Blocks : ProgressBarStyle.Marquee;
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            StatusTela(true);
+        }
+
+        private void process_OutputDataReceived(object sender, DataReceivedEventArgs e) {
+            this.Invoke((MethodInvoker)delegate {
+                textBox1.AppendText(e.Data + "\r\n");
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            process.Kill();
+            button2.Enabled = false;
+        }
+    }
+}
